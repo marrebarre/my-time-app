7df52480781cfcb208d3210ed147606519de1dc9
marrebarre <martiin98@gmail.com>
2022-04-04 18:55:54 +0200

    init and nav

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+      <Top />
+      <Home />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from "./components/Home.vue";
+import Top from "./components/Top.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    Home,
+    Top,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.v-application {
+  background-color: rgb(112, 112, 112);
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-card></v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({}),
+};
+</script>
+
+<style scoped>
+.v-card {
+  min-height: 860px;
+}
+</style>
